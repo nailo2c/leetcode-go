fb76eb910e1ccd4b4adb26eb2c75bc606c767ec3
nailo2c <nailo2c@gmail.com>
2020-01-18 16:30:57 +0800

    add solution of Roman to Integer

---
+++ problems/0013_Roman_to_Integer/roman_to_integer.go
@@ -0,0 +1,36 @@
+package problem0013
+
+func romanToInt(s string) int {
+	m := map[string]int{
+		"I":  1,
+		"IV": 4,
+		"V":  5,
+		"IX": 9,
+		"X":  10,
+		"XL": 40,
+		"L":  50,
+		"XC": 90,
+		"C":  100,
+		"CD": 400,
+		"D":  500,
+		"CM": 900,
+		"M":  1000,
+	}
+
+	var output int = 0
+	var temp string = "M"
+	for _, char := range s {
+		c := string(char)
+
+		if m[temp] < m[c] {
+			output -= m[temp]
+			output += m[temp+c]
+		} else {
+			output += m[c]
+		}
+
+		temp = c
+	}
+
+	return output
+}
