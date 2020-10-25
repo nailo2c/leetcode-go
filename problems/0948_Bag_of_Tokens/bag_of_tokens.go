314494810f625ebfccfb7268beb6f13426230d83
nailo2c <nailo2c@gmail.com>
2020-10-25 22:18:35 +0800

    fix pipeline error

--- problems/0948_Bag_of_Tokens/bag_of_tokens.go
+++ problems/0948_Bag_of_Tokens/bag_of_tokens.go
@@ -1,19 +1,21 @@
+package problem0948
+
 import "sort"
 
 func bagOfTokensScore(tokens []int, P int) int {
-    score := 0
-    sort.Slice(tokens, func(i, j int) bool { return tokens[i] < tokens[j] })
-    for len(tokens) > 0 && P >= tokens[0] {
-        P = P - tokens[0]
-        score++
-        tokens = tokens[1:]
-        
-        if len(tokens) > 1 && P < tokens[0] {
-            P = P + tokens[len(tokens)-1]
-            score--
-            tokens = tokens[:len(tokens)-1]
-        }
-    }
-    
-    return score
+	score := 0
+	sort.Slice(tokens, func(i, j int) bool { return tokens[i] < tokens[j] })
+	for len(tokens) > 0 && P >= tokens[0] {
+		P = P - tokens[0]
+		score++
+		tokens = tokens[1:]
+
+		if len(tokens) > 1 && P < tokens[0] {
+			P = P + tokens[len(tokens)-1]
+			score--
+			tokens = tokens[:len(tokens)-1]
+		}
+	}
+
+	return score
 }
